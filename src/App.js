f28b1c42ff9bf32dcaa6cde42e5a9871d11317df
Guilherme Reinaldo <hausofguir@gmail.com>
2022-10-04 22:30:30 -0400

    menu categorias

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
+import { useState } from "react";
 import Cabecalho from "./componentes/Cabecalho";
 import Lancamentos from "./componentes/Lancamentos";
 import MaisVendidos from "./componentes/MaisVendidos";
 import Rodape from "./componentes/Rodape";
+import MenuCategorias from "./componentes/MenuCategorias";
 
 function App() {
+const [menuCategoriasAtivo, setMenuCategoriasAtivo] = useState(false);
+
+
   return (
     <div>
-      <Cabecalho />
+      <Cabecalho menuCategoriasAtivo={menuCategoriasAtivo} setMenuCategoriasAtivo={setMenuCategoriasAtivo} />
       <Lancamentos />
       <MaisVendidos />
       <Rodape />
+      {menuCategoriasAtivo === true ? <MenuCategorias /> : ""}
     </div>
   );
 }
--- src/componentes/Cabecalho/index.js
+++ src/componentes/Cabecalho/index.js
@@ -1,15 +1,14 @@
 import { Person, ShoppingCart } from '@mui/icons-material';
 import "./Cabecalho.css";
 
-
-export default function Cabecalho() {
+export default function Cabecalho({ setMenuCategoriasAtivo, menuCategoriasAtivo }) {
     return (
         <div className="cabecalho"> 
             <img className="cabecalho__logo" src="/imagens/kpop4u1.png" alt="Logo da Kpop4u" />
             <p className="cabecalho__item">Início</p>
-            <p className="cabecalho__item">Categorias</p>
+            <p className="cabecalho__item" onClick={() => setMenuCategoriasAtivo(!menuCategoriasAtivo)}>Categorias</p>
             <p className="cabecalho__item">Sobre Nós</p>
-            <p className="cabecalho__item">Contato</p>
+            <p className="cabecalho__item">Contato</p>-
             <Person className="cabecalho__perfil" />
             <ShoppingCart className="cabecalho__carrinho" />
         </div>
