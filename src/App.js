5e4bfac2bd339bafdd62a3461ce34cdc0e1b4b54
Guilherme Reinaldo <hausofguir@gmail.com>
2022-10-05 21:50:51 -0400

    modal login

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import Lancamentos from "./componentes/Lancamentos";
 import MaisVendidos from "./componentes/MaisVendidos";
 import Rodape from "./componentes/Rodape";
 import MenuCategorias from "./componentes/MenuCategorias";
+import Modal from "./componentes/Modal";
 
 function App() {
 const [menuCategoriasAtivo, setMenuCategoriasAtivo] = useState(false);
+const [loginAtivo, setLoginAtivo] = useState(false);
 
 
   return (
     <div>
-      <Cabecalho menuCategoriasAtivo={menuCategoriasAtivo} setMenuCategoriasAtivo={setMenuCategoriasAtivo} />
+      <Cabecalho setLoginAtivo={setLoginAtivo} menuCategoriasAtivo={menuCategoriasAtivo} setMenuCategoriasAtivo={setMenuCategoriasAtivo} />
       <Lancamentos />
       <MaisVendidos />
       <Rodape />
       {menuCategoriasAtivo === true ? <MenuCategorias /> : ""}
+      {loginAtivo === true ? <Modal setLoginAtivo={setLoginAtivo} /> : ""}
     </div>
   );
 }
